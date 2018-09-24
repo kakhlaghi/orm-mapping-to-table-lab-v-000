d74f8908248d025f7bcc6e2149da2da0a7e82c92
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-24 00:37:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,4 +6,14 @@ class Student
   def initialize(name, grade, id = nil)
 
   end
+
+  def self.create_table
+    sql =  <<-SQL
+     CREATE TABLE IF NOT EXISTS songs (
+       id INTEGER PRIMARY KEY,
+       name TEXT,
+       grade TEXT
+       )
+       SQL
+  end
 end
